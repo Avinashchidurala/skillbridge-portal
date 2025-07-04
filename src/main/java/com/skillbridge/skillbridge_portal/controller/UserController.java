69d09f8fe4add525d85cca78488db819b2d59db9
Avinashchidurala <chiduralaavinash@gmail.com>
2025-07-04 20:04:37 +0530

    Added UserController with registration, activation, and media update APIs

--- src/main/java/com/skillbridge/skillbridge_portal/controller/UserController.java
+++ src/main/java/com/skillbridge/skillbridge_portal/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.skillbridge.skillbridge_portal.controller;
 
 import com.skillbridge.skillbridge_portal.model.User;
-import com.skillbridge.skillbridge_portal.repository.UserRepository;
+import com.skillbridge.skillbridge_portal.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
@@ -11,14 +13,31 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
-    @PostMapping("/create")
-    public User createUser(@RequestBody User user) {
-        // Auto-activate for test only
-        user.setActive(true);
-        user.setVerified(true);
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        user.setVerified(false);
+        user.setActive(false);
         user.setUniqueId("UID-" + System.currentTimeMillis());
-        return userRepository.save(user);
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/email/{email}")
+    public Optional<User> getUserByEmail(@PathVariable String email) {
+        return userService.findByEmail(email);
+    }
+
+    @PutMapping("/activate/{userId}")
+    public void setActivation(@PathVariable Long userId, @RequestParam boolean status) {
+        userService.setUserActivation(userId, status);
+    }
+
+    @PutMapping("/upload/{userId}")
+    public void updateMedia(@PathVariable Long userId,
+                            @RequestParam String imageUrl,
+                            @RequestParam String resumeUrl) {
+        userService.updateUserMediaUrls(userId, imageUrl, resumeUrl);
     }
 }
